f74e8b2f3f006d633ece5364872830eefc105047
Sam <sam210723@hotmail.com>
2020-09-20 13:25:24 +1000

    Update TSDuck scripts

--- scripts/mpe-tx.bat
+++ scripts/mpe-tx.bat
@@ -2,6 +2,5 @@
 title MPEG-TS MPE Re-transmitter
 
 REM Open input transport stream, slow bitrate down to actual off-air bitrate, handle encapsulation and output UDP datagrams on 239.0.0.1:8001, then drop TS packets
-:start
-tsp -I file "E:\RF\HimawariCast\154.0E_4148H_2586_(2020-07-06 0936 UTC).ts" -P regulate --bitrate 3075700 -P mpe --pid 0x03E9 --udp-forward --log -O drop
-goto start
+tsp -I file --infinite "E:\RF\HimawariCast\154.0E_4148H_2586_(2020-07-06 0936 UTC).ts" -P regulate --bitrate 3075700 -P mpe --pid 0x03E9 --udp-forward --log -O drop
+
