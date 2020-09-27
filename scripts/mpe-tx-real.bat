cb45b3cc5a1709d1e1534a5102666742866aadb6
Sam <sam210723@hotmail.com>
2020-09-27 18:05:01 +1000

    Update replay scripts

---
+++ scripts/mpe-tx-real.bat
@@ -0,0 +1,5 @@
+@echo off
+title MPEG-TS MPE Re-transmitter
+
+REM Open input transport stream, slow bitrate down to actual off-air bitrate (3075700 bps), handle encapsulation and output UDP datagrams on 239.0.0.1:8001, then drop TS packets
+tsp -I file --infinite "E:\RF\HimawariCast\TS\154.0E_4148H_2586_(2020-09-20 1121 UTC)_6983.ts" -P regulate --bitrate 3075700 -P mpe --pid 0x03E9 --udp-forward --log -O drop
