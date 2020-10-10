8e408d9d0a741fb5028952930bc577e2c4ec18d6
Sam <sam210723@hotmail.com>
2020-10-10 23:12:32 +1000

    Change TS recording

--- scripts/mpe-tx-real.bat
+++ scripts/mpe-tx-real.bat
@@ -2,4 +2,4 @@
 title MPEG-TS MPE Re-transmitter
 
 REM Open input transport stream, slow bitrate down to actual off-air bitrate (3075700 bps), handle encapsulation and output UDP datagrams on 239.0.0.1:8001, then drop TS packets
-tsp -I file "E:\RF\HimawariCast\ts\154.0E_4148H_2586_(2020-07-06 0936 UTC)_FILTERED.ts" -P regulate --bitrate 3075700 -P mpe --pid 0x03E9 --udp-forward --log -O drop
+tsp -I file "E:\RF\HimawariCast\ts\154.0E_4148H_2586_(2020-09-20 1136 UTC)_6903X_FILTERED.ts" -P regulate --bitrate 3075700 -P mpe --pid 0x03E9 --udp-forward --log -O drop
